df685ec4202c816fa4357a993129da4e135b27ac
DevGuivieiraa <Devguivieiraa@gmail.com>
2024-10-17 03:02:43 -0300

    .

---
+++ BackEnd/index.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA-s1v9bLIDkpvB1Gp8LQJ04k1uUT2W9HQ",
+    authDomain: "teste-projeto-a7fc2.firebaseapp.com",
+    projectId: "teste-projeto-a7fc2",
+    storageBucket: "teste-projeto-a7fc2.appspot.com",
+    messagingSenderId: "392555396843",
+    appId: "1:392555396843:web:2190a8401b5e6148c288ae"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  function Cadastrar(){
+    var auth = null;
+
+    firebase.auth().createUser
+  }
